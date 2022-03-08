8b27e184157ce9747b24205a5ad4686d96371c53
Pablo Vinícius Sousa Silva <48532322+PabloViniciusSS@users.noreply.github.com>
2022-03-08 16:31:32 -0300

    atualizaçoes

--- src/components/Buttom.jsx
+++ src/components/Buttom.jsx
@@ -1,4 +1,4 @@
 import React from "react";
 import "./Button.css";
 
-export default (props) => <button className="button">{props.label}</button>;
+export default props => <button className="button">{props.label}</button>;
